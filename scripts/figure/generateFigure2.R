96626e188e594cd335ef404f848ede28cae544cf
mbelitz <mbelitz@ufl.edu>
2024-09-06 14:33:55 -0400

    include year 1985

--- scripts/figure/generateFigure2.R
+++ scripts/figure/generateFigure2.R
@@ -2,7 +2,7 @@ library(tidyverse)
 
 preds <- read.csv(file = "data/weatherData/predictor_vars.csv")
 
-prec <- ggplot(filter(preds, year >= 1986 & year <= 2021)) +
+prec <- ggplot(filter(preds, year >= 1985 & year <= 2021)) +
   aes(x = year) +
   geom_path(aes(y = prcp_dry), color = "darkgoldenrod") +
   geom_smooth(aes(y = prcp_dry), method = "lm", color = "darkgoldenrod") +
@@ -16,7 +16,7 @@ prec <- ggplot(filter(preds, year >= 1986 & year <= 2021)) +
         axis.title=element_text(size=13.5))
 prec
 
-temp <- ggplot(filter(preds, year >= 1986 & year <= 2021)) +
+temp <- ggplot(filter(preds, year >= 1985 & year <= 2021)) +
   aes(x = year) +
   geom_path(aes(y = tmax_larvae), color = "red") +
   geom_smooth(aes(y = tmax_larvae), method = "lm", color = "red") +
@@ -30,7 +30,7 @@ temp <- ggplot(filter(preds, year >= 1986 & year <= 2021)) +
         axis.title=element_text(size=13.5))
 temp
 
-maxWind <- ggplot(filter(preds, year >= 1986 & year <= 2021)) +
+maxWind <- ggplot(filter(preds, year >= 1985 & year <= 2021)) +
   aes(x = year) +
   geom_path(aes(y = max_wind)) +
   geom_smooth(aes(y = max_wind), method = "lm") + 
@@ -40,7 +40,7 @@ maxWind <- ggplot(filter(preds, year >= 1986 & year <= 2021)) +
         axis.title=element_text(size=13.5))
 maxWind
 
-EWE <- ggplot(filter(preds, year >= 1986 & year <= 2021)) +
+EWE <- ggplot(filter(preds, year >= 1985 & year <= 2021)) +
   aes(x = year) +
   geom_path(aes(y = EWE_wind)) +
   geom_smooth(aes(y = EWE_wind), method = "lm") + 
@@ -51,7 +51,10 @@ EWE <- ggplot(filter(preds, year >= 1986 & year <= 2021)) +
 EWE
 
 
-cp <- cowplot::plot_grid(temp, prec, EWE, maxWind, labels = c("B", "C", "D", "E"))
+cp <- cowplot::plot_grid(temp, prec, EWE, maxWind, 
+                         labels = c("B", "C", "D", "E"),
+                         ncol = 2, nrow = 2, 
+                         label_y = 1.01)
 
 cp
 
@@ -68,8 +71,12 @@ tbl_fread2 <- tbl_fread %>%
   mutate(dif = ucl - lcl)
 
 popEstP <- ggplot(tbl_fread2) +
-  geom_smooth(mapping = aes(x = year, y = estimate), method = "lm", 
-              linetype = "dashed", color = "grey10", fill = "grey80", alpha = 0.7) +
+#  geom_smooth(mapping = aes(x = year, y = estimate), method = "glm.nb", 
+#              linetype = "dashed", color = "grey10", fill = "grey80", alpha = 0.7) +
+#  geom_rect(data = rects, 
+#            mapping = aes(xmin = xstart, xmax = xend, 
+#                          ymin = 0, ymax = Inf),
+#            fill = "snow3", alpha = 0.4) +
   geom_point(mapping = aes(x = year, y = estimate)) +
   geom_line(mapping = aes(x = year, y = estimate), alpha = 0.3) +
   geom_errorbar(mapping = aes(x = year, ymin = lcl, ymax = ucl)) +
@@ -80,9 +87,40 @@ popEstP <- ggplot(tbl_fread2) +
         axis.title=element_text(size=13.5))
 popEstP
 
-cp2 <- cowplot::plot_grid(popEstP, cp, 
+popEstP2 <- popEstP + 
+  geom_vline(xintercept = 2010, linetype = "dotted", color = "#219ebc") +
+  geom_vline(xintercept = 2005, linetype = "dotted", color = "#219ebc") +
+  geom_vline(xintercept = 1992, linetype = "dotted", color = "#219ebc") +
+  geom_vline(xintercept = 1991, linetype = "dotted", color = "#219ebc") +
+  geom_vline(xintercept = 1986, linetype = "dotted", color = "#219ebc") +
+  
+  geom_label(aes(x = 2010, y = 10500, label = "Bonnie\nTS"), fill = "white") + 
+  geom_label(aes(x = 2004.5, y = 60, label = "Katrina\nH1"), fill = "white") + 
+  geom_label(aes(x = 1991, y = 10500, label = "Fabian\nTS"), fill = "white") +
+  geom_label(aes(x = 1992.5, y = 60, label = "Andrew\nH5"), fill = "white") +
+  geom_label(aes(x = 1986, y = 60, label = "Floyd\nH1"), fill = "white") 
+  
+
+popEstP3 <- popEstP + 
+  geom_vline(xintercept = 2017, linetype = "dotted", color = "#219ebc") +
+  geom_vline(xintercept = 2005, linetype = "dotted", color = "#219ebc") +
+  geom_vline(xintercept = 1992, linetype = "dotted", color = "#219ebc") +
+  
+  geom_label(aes(x = 2017, y = 50, label = "Irma"), fill = "white") + 
+  geom_label(aes(x = 2005, y = 50, label = "Wilma"), fill = "white") + 
+  geom_label(aes(x = 1992, y = 50, label = "Andrew"), fill = "white") 
+
+popEstP3
+
+rects <- data.frame(release = c(1,2),
+                    xstart = c(1995,2014),
+                    xend = c(1997,2021))
+
+popEstP3 
+  
+cp2 <- cowplot::plot_grid(popEstP3, cp, 
                           nrow = 2, ncol = 1, labels = c("A", ""),
                           rel_heights = c(0.7,1))
 
-ggsave(filename = "Figures/pop_est&weatherCovariates.png", 
-       width = 6, height = 8, dpi = 450)
+ggsave(filename = "Figures/pop_est&weatherCovariates_annotated.png", plot = cp2,
+       width = 7, height = 8, dpi = 450)
